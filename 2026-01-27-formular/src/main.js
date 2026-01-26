2388663c13794aedc3a5781eb6bcc678fee45959
Jenni <jenni@posh.se>
2026-01-26 20:25:01 +0200

    formulär

---
+++ 2026-01-27-formular/src/main.js
@@ -0,0 +1,8 @@
+import './style.scss';
+
+/**
+ * 📚 Lästips kring formulär, tillgänglighet & design
+ * - https://www.nngroup.com/articles/errors-forms-design-guidelines/
+ * - https://www.smashingmagazine.com/2022/08/error-messages-ux-design/
+ * - https://www.inclusion.hsbc.com/en/articles/common-mistakes/error-messages-in-forms
+ **/
