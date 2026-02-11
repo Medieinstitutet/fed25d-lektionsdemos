1992bff88d3b83cf8e0402d09b5b3d805022b0e4
Jenni <jenni@posh.se>
2026-02-11 11:20:31 +0200

    ts interface + annat

---
+++ 2026-01-28-specialregler/vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/inl-2-budget/',
+});
---
+++ 2026-02-11-interface-etc/src/models.ts
@@ -0,0 +1,5 @@
+export interface IBudgetItem {
+  desc: string;
+  amount: string;
+  type: 'expense' | 'income';
+}
